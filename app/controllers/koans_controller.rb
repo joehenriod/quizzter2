0f50d60a7c807083423478f42270d0405b9103b0
Joe Henriod <joehenriod@gmail.com>
2015-09-07 19:59:32 +0000

    Koan Adder Form

--- app/controllers/koans_controller.rb
+++ app/controllers/koans_controller.rb
@@ -1,7 +1,20 @@
 class KoansController < ApplicationController
 	def index
-	
-  	@koan = Koan.order("RANDOM()").first
-
+  		@koan = Koan.order("RANDOM()").first
 	end
+
+	def new
+		@koan = Koan.new
+	end
+
+	def create
+		Koan.create(koan_params)
+		redirect_to root_path
+	end
+
+	private
+
+  def koan_params
+    params.require(:koan).permit(:saying, :author)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Quizzter2::Application.routes.draw do
 
   root 'koans#index'
+
+  resources :koans
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
