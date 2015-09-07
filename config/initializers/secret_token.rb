47d2ba38c113d5cfaf137fcbeff4aedf9f904c41
Joe Henriod <joehenriod@gmail.com>
2015-09-07 18:17:56 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quizzter2::Application.config.secret_key_base = 'e81be4fcfe481a2ffe351f0577ffd893465a5149cbd1cd83f197b9358d3da1ebd17b73239c0eb84f9640b4fa2578846d68766c22106d065ef39b837ec8ed8332'
